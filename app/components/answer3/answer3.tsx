7e9877e857f7f4f017757ab69181613c91336816
BrunoCarvalhoCagni <brunocarvalhocagni@outlook.com>
2025-09-01 08:55:08 -0300

    question 3 finish

--- app/components/answer3/answer3.tsx
+++ app/components/answer3/answer3.tsx
@@ -12,7 +12,36 @@ export function Answer3() {
           <div className='question3-header'>
             <p>Header</p>
           </div>
-          <div className='question3-col-1'></div>
+          <div className='question3-row'>
+            <div className='question3-col first'>
+              <div className='question3-hero'>
+                <p>Hero</p>
+              </div>
+              <div className='question3-sidebar'>
+                <p>Sidebar</p>
+              </div>
+            </div>
+            <div className='question3-col second'>
+              <div className='question3-main-content'>
+                <p>Main Content</p>
+                <p className='small'>**If things do not look right make sure your browser <br />is in "Experimental Mode"</p>
+              </div>
+              <div className='question3-extra-content'>
+                <p>Extra Content</p>
+              </div>
+            </div>
+          </div>
+          <div className='question3-row'>
+            <div className='question3-related-images'>
+              Related Images
+            </div>
+            <div className='question3-related-posts'>
+              Related Posts
+            </div>
+          </div>
+          <div className='question3-footer'>
+            <p>Footer</p>
+          </div>
         </div>
         </div>
     </main>
